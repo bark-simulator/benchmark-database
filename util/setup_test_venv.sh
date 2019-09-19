7d1d43e890c137cc7c53f73f1bd4d32e85339f7f
jbernhard <bernhard@fortiss.org>
2019-09-19 17:09:32 +0200

    fix setup venv script

--- util/setup_test_venv.sh
+++ util/setup_test_venv.sh
@@ -5,7 +5,7 @@ mkdir -p $DIR/ghr/
 virtualenv --system-site-packages -p python3 $DIR/venv
 cd /tmp
 wget https://github.com/tcnksm/ghr/releases/download/v0.13.0/ghr_v0.13.0_linux_386.tar.gz
-sudo tar -zxvf ghr_v0.13.0_linux_386.tar.gz -C $DIR/ghr/
+tar -zxvf ghr_v0.13.0_linux_386.tar.gz -C $DIR/ghr/
 cd $DIR
 export PATH=$PATH:$DIR/ghr/ghr_v0.13.0_linux_386/
 source $DIR/venv/bin/activate
